7f17f6c17ff620261397c6f98add67a9637af440
Alex Mark <ajmark@andrew.cmu.edu>
2014-06-07 17:32:48 -0700

    grid block

--- index.php
+++ index.php
@@ -22,15 +22,15 @@
                             <ul class="right">
                                 <li class="divider"></li>
                                 <li>
-                                    <a href="#" class="view-link-small" value="work-experience">Work Experience</a>
+                                    <a class="view-link-small" value="work-experience">Work Experience</a>
                                 </li>
                                 <li class="divider"></li>
                                 <li>
-                                    <a href="#" class="view-link-small" value="projects">Projects</a>
+                                    <a class="view-link-small" value="projects">Projects</a>
                                 </li>
                                 <li class="divider"></li>
                                 <li>
-                                    <a href="#" class="view-link-small" value="resume">Resume</a>
+                                    <a class="view-link-small" value="resume">Resume</a>
                                 </li>
                             </ul>
                         </section>
@@ -42,25 +42,25 @@
             <div class="row view-control show-for-medium-up">
                 <ul class="inline-list text-center view-control-list">
                     <li>
-                        <a id="projects-link" class="view-link" href="#" value='projects'>Projects</a>
+                        <a id="projects-link" class="view-link" value='projects'>Projects</a>
                     </li>
                     <li>
                         <p>|</p>
                     </li>
                     <li>
-                        <a id="work-experience-link" class="view-link active" href="#" value='work-experience'>Work Experience</a>
+                        <a id="work-experience-link" class="view-link active" value='work-experience'>Work Experience</a>
                     </li>
                     <li>
                         <p>|</p>
                     </li>
                     <li>
-                        <a id="resume-link" class="view-link" href="#" value='resume'>Resume</a>
+                        <a id="resume-link" class="view-link" value='resume'>Resume</a>
                     </li>
                 </ul>
             </div>
 
             <div class="content-box-middle visible" id="work-experience">
-                <div class="small-12 medium-10 medium-centered content-body columns">
+                <div class="small-12 medium-10 medium-centered content-body">
                     <div class="row">
                         <h2 class="hide-for-medium-up columns">Work Experience</h2>
                         <div class="medium-4 columns">
@@ -75,7 +75,7 @@
                     </div>
                 </div>
 
-                <div class="small-12 medium-10 medium-centered content-body columns">
+                <div class="small-12 medium-10 medium-centered content-body">
                     <div class="row">
                         <div class="medium-4 columns">
                             <img src="public/assets/bnymellon_logo.png" class="work-logo">
@@ -91,23 +91,63 @@
             </div>
 
             <div class="content-box-right" id="resume">
-                <h2 class="hide-for-medium-up">Resume</h2>
-                <p> Resume. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="small-12 content-body">
+                    <div class="row">
+                        <div class="small-12 medium-4 columns">
+                            <h2 class="hide-for-medium-up">Resume</h2>
+                            <h4>Alex Mark</h4>
+                            <h6>ajmark@andrew.cmu.edu</h6>
+
+                            <a href="#" class="button tiny">Open PDF Resume</a>
+
+                            <ul class="inline-list">
+                                <li>
+                                    <a class='social' href='http://www.linkedin.com/pub/alexander-mark/58/9b/605' target='_blank'>LinkedIn</a>
+                                </li>
+                                <li>    
+                                    <a class='social' href='http://www.github.com/ajmark' target='_blank'>GitHub</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="medium-8 columns show-for-medium-up">
+
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div class="content-box-left" id="projects">
-                <h2 class="hide-for-medium-up">Projects</h2>
-                <p> Projects. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="small-12 content-body">
+                    <div class="row">
+                        <div class="small-12">
+                            <h2 class="hide-for-medium-up">Projects</h2>
+                            <ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-4">
+                                
+                                <li>
+                                    <div class="project-box">
+                                    </div>
+                                </li>
+                                
+                                <li>
+                                    <div class="project-box">
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <div class="project-box">
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <div class="project-box">
+                                    </div>
+                                </li>
+                                                                
+                            </ul>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </body>
